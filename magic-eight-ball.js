dc8e1e774326bcc2e01e4534e031fa99fdd62b3e
jfraboni <johnfraboni@hotmail.com>
2014-06-22 17:22:03 -0400

    * update app

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
+// TODO 1 : create variables for prompt and fs by calling require in each respective module:
 var 
     prmpt = require('prompt'),
     fs = require('fs');
 
+// TODO 2 : create a welcomeMessage variable and log the welcomeMessage:
 var welcomeMessage = "Magic Eight Ball"
 console.log(welcomeMessage);
 
+// TODO 3 : create two variables, the answers array and the file path to the answers.json:
 var answers = [];
 var file = __dirname + '/answers.json';
 
-fs.readFile(file, 'utf8', function (err, data) {
-  if (err) {
-    console.log('Error: ' + err);
-    return;
-  }
 
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) { return onErr(err); }
+  
   data = JSON.parse(data);
+  
+  // TODO 4 : Loop through the data.answers objects, and push answer-records into the answers array:
   for (index in data.answers) {
       answers.push(data.answers[index].value);
   }
 
+  // TODO 5 : Invoke the promptForQuestion() function:
   promptForQuestion();
 });
 
@@ -39,10 +43,13 @@ function promptForQuestion() {
 
 	prmpt.get(properties, function (err, input) {
 		if (err) { return onErr(err); }
+
+    // TODO 7 : Invoke the showResponse() function, passing in a random index from the answers array:
 		showResponse(randomNumberBetween(0, answers.length-1));
 	});	
 }
 
+// TODO 6 : Create the showResponse() function:
 function showResponse(index) {
 	console.log('The Magic Eight Ball responds: ' + answers[index]);
 }
